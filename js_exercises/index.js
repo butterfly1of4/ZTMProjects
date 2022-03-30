87dbc426a50cf7eae3777449405de96b99af678e
butterfly1of4 <jhouck29@gmail.com>
2022-03-30 10:54:52 -0400

    added js exercises & solutions

---
+++ js_exercises/index.js
@@ -0,0 +1,58 @@
+/*Exercise 3*/
+
+/*
+var numOne = prompt("enter a number");
+var numTwo = prompt("enter another number");
+var sign = prompt("enter a sign");
+var sum = numOne + numTwo;
+var product = numOne * numTwo;
+var quotient = numOne / numTwo;
+var answer = numOne - numTwo;
+
+var total;
+
+switch (sign) {
+  case "+":
+    total = sum;
+    break;
+  case "-":
+    total = answer;
+    break;
+  case "*":
+    total = product;
+    break;
+  case "/":
+    total = quotient;
+    break;
+  default:
+    alert("please enter again");
+}
+console.log(total);
+if (sign == "+") {
+  alert(sum);
+} else if (sign == "-") {
+  alert(answer);
+} else if (sign == "*") {
+  alert(product);
+} else if (sign == "/") {
+  alert(quotient);
+} else {
+  alert("that didn't work");
+}
+*/
+
+/*Exercise 4*/
+
+
+var driverAge = prompt("Enter your age: ")
+
+if(driverAge < 18){
+    alert("Sorry, you are too young to drive this car. Powering off")
+} else if(driverAge> 18){
+    alert("Congratulations on your first year of driving. Enjoy the ride! Car is Powering On. Enjoy the ride!")
+} else{
+    alert("Please enter again")
+}
+
+/*Exercise 5*/
+
